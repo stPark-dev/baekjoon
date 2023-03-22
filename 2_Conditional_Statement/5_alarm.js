db7156f9c0f38e4cfd0f437ab981ccbde69a1b47
stPark-dev <dmc6618@gmail.com>
2023-03-22 23:56:32 +0900

    commit

--- 2_Conditional_Statement/5_alarm.js
+++ 2_Conditional_Statement/5_alarm.js
@@ -1,7 +1,7 @@
 // 원래 설정되어 있는 알람 시간에서 45분 앞서는 시간을 구하는 프로그램을 작성해라
 // 예시 : 10 10  -> 9 25
 
-let fs = require('fs')
+let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 
 let h = Number(input[0])
